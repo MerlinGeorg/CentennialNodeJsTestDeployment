f346a37d10713e90f9c6727982170accb98f1157
MerlinGeorg <merlingeo96@gmail.com>
2024-11-23 09:59:06 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+app.get ('/', (req,res) => {
+    res.send("hello world")
+});
+
+app.get('/test', (req,res) => {
+    res.send("hello world test")
+});
+
+const PORT = 8000;
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`)
+})
